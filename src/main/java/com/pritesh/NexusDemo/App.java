6ea8672096af5c16a42c54f8062038d496053b86
BcDemir <burak.demir@deop.ca>
2023-07-31 19:54:13 -0400

    first commit

---
+++ src/main/java/com/pritesh/NexusDemo/App.java
@@ -0,0 +1,13 @@
+package com.pritesh.NexusDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
